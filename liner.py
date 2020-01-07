217c882ec32d29f3c89519cbd4960bd08d69eded
vchernova12 <58112183+vchernova12@users.noreply.github.com>
2020-01-07 11:44:09 +0300

    Add files via upload

--- liner.py
+++ liner.py
@@ -16,26 +16,26 @@ Billboard1 = pulp.LpVariable("Billboard1", 2, 10, cat=pulp.LpInteger)
 Billboard2 = pulp.LpVariable("Billboard2", 2, 8, cat=pulp.LpInteger)
 Billboard3 = pulp.LpVariable("Billboard3", 2, 20, cat=pulp.LpInteger)
 
-Difference_constaint_BB1_BB2 = {"Billboard1": 1.0,
-                                "Billboard2": 1.0,
+Difference_constraint_BB1_BB2 = {"Billboard1": 1.0,
+                                "Billboard2": -1.0,
                                 "Billboard3": 0.0}
 
-Difference_constaint_BB1_BB3 = {"Billboard1": 1.0,
+Difference_constraint_BB1_BB3 = {"Billboard1": -1.0,
                                 "Billboard2": 0.0,
                                 "Billboard3": 1.0}
 
-Difference_constaint_BB2_BB3 = {"Billboard1": 0.0,
-                                "Billboard2": 1.0,
+Difference_constraint_BB2_BB3 = {"Billboard1": 0.0,
+                                "Billboard2": -1.0,
                                 "Billboard3": 1.0}
 
 prob = pulp.LpProblem("The number of display problem", pulp.LpMaximize)
 ingredient_vars = pulp.LpVariable.dicts("Ingr", Billboards, 2, cat= pulp.LpInteger)
 # Objective Function 
 prob += pulp.lpSum([costs[i]*ingredient_vars[i] for i in Billboards])
-
-#prob += pulp.lpSum([Difference_constaint_BB1_BB2[i] * ingredient_vars[i] for i in Billboards]) <= 2
-prob += pulp.lpSum([Difference_constaint_BB1_BB3[i] * ingredient_vars[i] for i in Billboards]) <= 10
-prob += pulp.lpSum([Difference_constaint_BB2_BB3[i] * ingredient_vars[i] for i in Billboards]) <= 12
+# Constraints: 
+prob += pulp.lpSum([Difference_constraint_BB1_BB2[i] * ingredient_vars[i] for i in Billboards]) <= 2
+prob += pulp.lpSum([Difference_constraint_BB1_BB3[i] * ingredient_vars[i] for i in Billboards]) <= 10
+prob += pulp.lpSum([Difference_constraint_BB2_BB3[i] * ingredient_vars[i] for i in Billboards]) <= 12
 prob += pulp.lpSum([contacts[i]*ingredient_vars[i] for i in Billboards]) <= 20000
 # The problem data is written to an .lp file
 prob.writeLP("The number of display problem.lp")
